648d8d0127fff5916b73f7ea1d397aaf03d90e78
tjstruck <tjstruck@arizona.edu>
2026-05-22 12:23:29 -0700

    Update validation

--- evaluation/validate.py
+++ evaluation/validate.py
@@ -10,36 +10,105 @@ written to a JSON file. This JSON file will then be used to annotate
 the submission (next step in the workflow CWL).
 """
 
-import argparse
-import csv
-import json
+import argparse, json
+
+# if args.submission_file is None:
+#     prediction_file_status = "INVALID"
+#     invalid_reasons = ['Expected FileEntity type but found ' + args.entity_type]
+# else:
+#     invalid_reasons = []
+#     prediction_file_status = "VALIDATED"
+
+#     try:
+#         open(args.submission_file, "r")
+#     except FileNotFoundError:
+#         invalid_reasons = ["File could not be opened"]
+
+#     if invalid_reasons == []:
+#         exc = 'No error'
+#         with open(args.submission_file) as stream:
+#             try:
+#                 fi = yaml.safe_load(stream)
+#             except yaml.YAMLError as exc:
+#                 invalid_reasons = [exc]
+
+#     if invalid_reasons == []:
+#         try:
+#             fi['parameters']['generations']
+#             fi['parameters']['post_decline_fraction']
+#         except KeyError:
+#             invalid_reasons = ['Could not find one or more parameters, which are required for scoring']
+
+#     if invalid_reasons != []:
+#         prediction_file_status = "INVALID"
+# result = {'submission_errors': "\n".join(invalid_reasons),
+#         'submission_status': prediction_file_status}
+# with open(args.results, 'w') as o:
+#     o.write(json.dumps(result))
 
 
-def validate_csv(filepath, delim=",", expected_cols=["id"]):
+
+
+
+def validate_yaml(filepath, expected_entries=["id"]):
     """
-    Checks for expected colnames in the CSV file.
+    Checks for expected colnames in the YAML file.
     """
+
     errors = []
-    with open(filepath) as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=delim)
-        for colname in expected_cols:
-            if colname not in reader.fieldnames:
-                errors.append(f"'{colname}' is missing from the prediction file")
-    return "\n".join(errors)
+    prediction_file_status = "VALIDATED"
 
+    try:
+        open(filepath, "r")
+    except FileNotFoundError:
+        errors = ["File could not be opened"]
+
+    if errors == []:
+        exc = 'No error'
+        with open(filepath) as stream:
+            try:
+                fi = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                errors = [exc]
+
+    if errors == []:
+        try:
+            for entry in expected_entries:
+                fi['parameters'][entry]
+        except KeyError:
+            errors = ['Could not find one or more parameters, which are required for scoring']
+
+    # if errors != []:
+    #     prediction_file_status = "INVALID"
+
+    # result = {'submission_errors': "\n".join(errors),
+    #         'submission_status': prediction_file_status}
+    # with open(args.results, 'w') as o:
+    #     o.write(json.dumps(result))
+
+    # Example from Synapse validation script template:
+    # errors = []
+    # with open(filepath) as yamlfile:
+    #     data = yaml.safe_load(yamlfile)
+    #     for colname in expected_entries:
+    #         if colname not in reader.fieldnames:
+    #             errors.append(f"'{colname}' is missing from the prediction file")
+    return "\n".join(errors)
 
 def main():
     """Main function."""
 
+    expected_entries=[
+                "generations",
+                "post_decline_fraction",
+                ]
+
     if args.entity_type != "FileEntity":
         errors = f"Submission should be a file, not {args.entity_type}"
     else:
-        errors = validate_csv(
+        errors = validate_yaml(
             args.prediction_file,
-            expected_cols=[
-                "PatientID",
-                "probability",
-            ],
+            expected_entries,
         )
 
     result = {
@@ -56,7 +125,7 @@ if __name__ == "__main__":
         "-p",
         "--prediction_file",
         required=True,
-        help="Filepath to prediction CSV",
+        help="Filepath to prediction YAML",
     )
     parser.add_argument(
         "-e",
--- steps/validate.cwl
+++ steps/validate.cwl
@@ -1,94 +1,56 @@
 #!/usr/bin/env cwl-runner
-
 cwlVersion: v1.0
 class: CommandLineTool
-label: Validate predictions file
+doc: Validate predictions prior to scoring
 
 requirements:
-  - class: InlineJavascriptRequirement
-  - class: InitialWorkDirRequirement
-    listing:
-    - entryname: validate.py
-      entry: |
-        #!/usr/bin/env python
-        import argparse
-        import json
-        import yaml
-        parser = argparse.ArgumentParser()
-        parser.add_argument("-r", "--results", required=True, help="validation results")
-        parser.add_argument("-e", "--entity_type", required=True, help="synapse entity type downloaded")
-        parser.add_argument("-s", "--submission_file", help="Submission File")
-
-        args = parser.parse_args()
-
-        if args.submission_file is None:
-            prediction_file_status = "INVALID"
-            invalid_reasons = ['Expected FileEntity type but found ' + args.entity_type]
-        else:
-            invalid_reasons = []
-            prediction_file_status = "VALIDATED"
-
-            try:
-                open(args.submission_file, "r")
-            except FileNotFoundError:
-                invalid_reasons = ["File could not be opened"]
-
-            if invalid_reasons == []:
-
-              exc = 'No error'
-              with open(args.submission_file) as stream:
-                  try:
-                      fi = yaml.safe_load(stream)
-                  except yaml.YAMLError as exc:
-                      invalid_reasons = [exc]
-
-            if invalid_reasons == []:
-                try:
-                    fi['parameters']['generations']
-                    fi['parameters']['post_decline_fraction']
-                except KeyError:
-                    invalid_reasons = ['Could not find one or more parameters, which are required for scoring']
-
-            if invalid_reasons != []:
-                prediction_file_status = "INVALID"
-        result = {'submission_errors': "\n".join(invalid_reasons),
-                  'submission_status': prediction_file_status}
-        with open(args.results, 'w') as o:
-            o.write(json.dumps(result))
+- class: InlineJavascriptRequirement
+- class: InitialWorkDirRequirement
+  listing:
+  - entryname: validate.py
+    entry:
+      $include: ../evaluation/validate.py
 
 inputs:
-  - id: input_file
-    type: File?
-  - id: entity_type
-    type: string
+- id: pred_file
+  type: File
+- id: groundtruth_file
+  type: File?
+  inputBinding:
+    prefix: -g
+- id: entity_type
+  type: string?
+  inputBinding:
+    prefix: -e
+- id: previous_annotation_finished
+  type: boolean?
 
 outputs:
-  - id: results
-    type: File
-    outputBinding:
-      glob: results.json
-  - id: status
-    type: string
-    outputBinding:
-      glob: results.json
-      outputEval: $(JSON.parse(self[0].contents)['submission_status'])
-      loadContents: true
-  - id: invalid_reasons
-    type: string
-    outputBinding:
-      glob: results.json
-      outputEval: $(JSON.parse(self[0].contents)['submission_errors'])
-      loadContents: true
+- id: results
+  type: File
+  outputBinding:
+    glob: results.json
+- id: status
+  type: string
+  outputBinding:
+    glob: results.json
+    outputEval: $(JSON.parse(self[0].contents)['submission_status'])
+    loadContents: true
+- id: invalid_reasons
+  type: string
+  outputBinding:
+    glob: results.json
+    outputEval: $(JSON.parse(self[0].contents)['submission_errors'])
+    loadContents: true
 
-baseCommand: python
+baseCommand:
+- python3
+- validate.py
 arguments:
-  - valueFrom: validate.py
-  - prefix: -s
-    valueFrom: $(inputs.input_file)
-  - prefix: -e
-    valueFrom: $(inputs.entity_type)
-  - prefix: -r
-    valueFrom: results.json
+- prefix: -p
+  valueFrom: $(inputs.pred_file.path)
+- prefix: -o
+  valueFrom: results.json
 
 hints:
   DockerRequirement:
