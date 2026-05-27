af37b74f3fc7342be9cb10f1cae130d905ec6cc4
tjstruck <tjstruck@arizona.edu>
2026-05-26 23:42:45 -0700

    Add check for proper entries

--- evaluation/validate.py
+++ evaluation/validate.py
@@ -72,35 +72,22 @@ def validate_yaml(filepath, expected_entries=["id"]):
                 errors = [exc]
 
     if errors == []:
-        try:
-            for entry in expected_entries:
+        for entry in expected_entries:
+            try:
                 fi['parameters'][entry]
-        except KeyError:
-            errors = ['Could not find one or more parameters, which are required for scoring']
+            except KeyError:
+                errors.append(f'Could not find {entry} parameters\n')
 
-    # if errors != []:
-    #     prediction_file_status = "INVALID"
-
-    # result = {'submission_errors': "\n".join(errors),
-    #         'submission_status': prediction_file_status}
-    # with open(args.results, 'w') as o:
-    #     o.write(json.dumps(result))
-
-    # Example from Synapse validation script template:
-    # errors = []
-    # with open(filepath) as yamlfile:
-    #     data = yaml.safe_load(yamlfile)
-    #     for colname in expected_entries:
-    #         if colname not in reader.fieldnames:
-    #             errors.append(f"'{colname}' is missing from the prediction file")
     return "\n".join(errors)
 
 def main():
     """Main function."""
 
     expected_entries=[
-                "generations",
-                "post_decline_fraction",
+                "AFR_population_size",
+                "EUR_population_size",
+                "generations_since_split",
+                "migration_rate",
                 ]
 
     # if args.entity_type != "FileEntity":
