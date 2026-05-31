25f0f368f0af8ada562930e84799cf6e6ddbb39b
tjstruck <tjstruck@arizona.edu>
2026-05-30 19:42:04 -0700

    Update validation

--- evaluation/validate.py
+++ evaluation/validate.py
@@ -59,12 +59,12 @@ def validate_table(filepath, truth):
             table['relation'] = table['relation'].str.lower()
         except:
             pass
-        potential_entries = '\n\n'.join(groundtruth['relation'].unique())
+        potential_entries = '\n'.join(groundtruth['relation'].unique())
         for ele in table['relation']:
             if ele not in groundtruth['relation'].unique():
                 errors.append(f"Unexpected relation value: {ele}. Expected relation of any of the following: " + potential_entries)
 
-    return "\n\n".join(errors)
+    return "\n".join(errors)
 
 def main():
     """Main function."""
